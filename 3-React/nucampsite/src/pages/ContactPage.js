e9ef0f105216ef0b29ae0a2de48912fdaa5b44f9
Jason Goodnight <Jasongoodnight3218@gmail.com>
2023-09-22 21:14:30 -0600

    ContactForm Parts 1 and 2

--- 3-React/nucampsite/src/pages/ContactPage.js
+++ 3-React/nucampsite/src/pages/ContactPage.js
@@ -1,3 +1,4 @@
+import ContactForm from '../components/ContactForm';
 import { Container, Col, Row } from 'reactstrap';
 import SubHeader from '../components/SubHeader';
 
@@ -41,7 +42,9 @@ const ContactPage = () => {
                     <h2>Send Us Your Feedback</h2>
                     <hr />
                 </Col>
-                <Col md='10'>TBD: ContactForm</Col>
+                <Col md='10'>
+                    <ContactForm />
+                </Col>
             </Row>
         </Container>
     );
